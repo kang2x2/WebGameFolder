da2902fb1d7802e736af6ee29585314b0b180e2f
DESKTOP-0E8L646\kang2 <kang2x2@naver.com>
2020-08-23 18:41:00 +0900

    2020-08-23 로또추첨기 js 업데이트 ver1

--- 로또추첨기/로또추첨기.js
+++ 로또추첨기/로또추첨기.js
@@ -1,30 +1,53 @@
 let num = document.querySelector('.number');
 
-let showNum;
-let loopNum = [];
-let answer = [];
+// 플레이어 변수
+let count = 0;
+let overlap = true;
+let pNum = Array(7);
 
-// 45개의 번호 저장.
-for(let i = 1; i <= 45; ++i) {
-    loopNum.push(i);
+// 컴퓨터 변수
+let comCount = 0;
+let comOverlap = true;
+let comNum = Array(7);
+
+// 컴퓨터 번호 뽑기
+while(comCount < 7) {
+    let comPick = Math.floor(Math.random() * 45 + 1);
+    for(let i = 0; i < comCount; ++i) {
+        if(comNum[i] === comPick) {
+            comOverlap = false;
+        }
+    }
+    if(comOverlap) {
+        comNum[comCount] = comPick;
+        comCount = comCount + 1;
+    }
+
+    comOverlap = true;
 }
-console.log(loopNum);
 
-// 컴퓨터가 6개의 번호 + 보너스 번호 1개 저장.
-for(let i = 0; i < 6; ++i) {
+// 플레이어 번호 뽑기
+while(count < 7) {
+    let pick = Math.floor(Math.random() * 45 + 1);
+    for(let i = 0; i < count; ++i) {
+        if(pNum[i] === pick) {
+            overlap = false;
+        }
+    }
+    if(overlap) {
+        pNum[count] = pick;
+        count = count + 1;
+    } 
 
+    overlap = true;
 }
 
-// 돌아가는 번호
-function temp() {
-   showNum = Math.floor(Math.random() * 45 + 1);
+// 컴퓨터와 플레이어의 번호 비교
+for(let i = 0; i < 7; ++i) {
+    
 }
 
-showTemp = setInterval(function(){  
-    temp();
-    num.innerHTML = showNum;
-}, 100);
-
-
+console.log('컴퓨터' + comNum);
+console.log('플레이어' + pNum);
 
 
